d901448f2000e422ddd914d4bc6254432d4a5459
Donnell Harris <donnell.harris@koodomobile.com>
2023-02-03 14:26:53 -0800

    nav bar mock-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+// import { Fragment } from 'react';
 
 function App() {
   return (
+    
+    
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = function(props) {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <span className="font-weight-bold">
+        Say "Start" to begin.
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;

6cc76d0a568d603d9f6a44d26e63f119c7cd3b66
Donnell Harris <donnell.harris@koodomobile.com>
2023-02-03 17:45:47 -0800

    add speech recogition to app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar'
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 // import { Fragment } from 'react';
+import React, { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+// fixed data for username
+const username = "Player 1";
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  // template for command handling
+  const commands = [
+    {
+      command: "reset",
+      callback: () => resetTranscript(),
+    },
+    {
+      command: "clear",
+      callback: () => resetTranscript(),
+    },
+    {
+      command: "Marco",
+      callback: () => setMessage("Polo?"),
+    },
+    {
+      command: "Ping",
+      callback: () => setMessage("Pong!"),
+    },
+    {
+      command: "Start" || "star",
+      callback: () => setMessage("Starting Adventure!"),
+    }
+  ];
+
+  const {
+    transcript,
+    interimTranscript,
+    finalTranscript,
+    resetTranscript,
+    listening,
+  } = useSpeechRecognition({ commands });
+
+  // converts speech to text and stores in finalTranscript
+  useEffect(() => {
+    if (finalTranscript !== "") {
+      console.log("Got final result:", finalTranscript);
+
+      resetTranscript();
+    }
+  }, [interimTranscript, finalTranscript]);
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null;
+  }
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    console.log(
+      "Your browser does not support speech recognition software! Try Chrome desktop, maybe?"
+    );
+  }
+
+  const listenContinuously = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "en-GB",
+    });
+  };
+
+  listenContinuously();
+
   return (
-    
-    
     <div className="App">
-      <Navbar />
+      <Navbar playerName={username} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          Input : {transcript}
+        </div>
+        <div>
+          Response : {message}
+        </div>
       </header>
     </div>
   );

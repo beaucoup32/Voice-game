51c6fe9cd0b9733715c47ad950d567b3ed6d7115
RickCarr <rick.carr@telus.com>
2023-02-06 20:45:19 -0800

    updating files to support last update without breaking other features

--- src/components/GameStart.jsx
+++ src/components/GameStart.jsx
@@ -1,12 +1,9 @@
-const GameStart = (props) => {
-  const transition = props.transition;
-  const WEEK_0 = "WEEK_0";  
-  const { transcript, resetTranscript } = props.useListen;
+const GameStart = () => {  
 
   return (
     <div className="container">
       <h2>Tell me your name :</h2>
-      {transcript}
+      
     </div>
   );
 };

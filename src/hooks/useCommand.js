11b236c33468b08c58451281f0284b037c4dfecc
Manzar <manzar.hussain@telus.com>
2023-02-07 13:01:35 -0500

    fixed bug

--- src/hooks/useCommand.js
+++ src/hooks/useCommand.js
@@ -120,7 +120,7 @@ export default function useCommand(props) {
       default:
         setCommands([]);
     }
-  }, [mode]);
+  }, [mode, handleTTS, player, resetTranscript, setPlayer, setResponse, transition]);
   return { commands, listenContinuously, transcript, resetTranscript };
 }
 

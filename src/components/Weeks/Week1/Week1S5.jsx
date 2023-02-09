cb15c1bf43aa9a90acf55b29749b6d02468d4e25
RickCarr <rick.carr@telus.com>
2023-02-08 23:35:10 -0800

    adding week1 scenario 5

---
+++ src/components/Weeks/Week1/Week1S5.jsx
@@ -0,0 +1,8 @@
+export default function Week1S5(props) {
+  const {scenario} = props;
+
+  return (    
+    <p>{scenario ? scenario : "You wade into some standing water. Look down and you see your legs covered in Leeches! You run out of the river like a pheonix! The Leeches break out in song. 🎵`Oh wont you stand by me... `🎵 Shocked, you find yourself standing in some wading water. Look down and see your legs covered in Leeches! You start to run again.. but keep your cool and realize you are caught in a Loop. Ok Corey Feldman, what will you Do/While you are here?"}</p>
+  );
+};
+
